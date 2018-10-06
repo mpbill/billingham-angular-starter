ff4e11423953c49e458a718a74f608a54ba0881c
Michael Billingham <michael.billingham@compeat.com>
2018-10-06 13:44:55 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
+import { HomePageComponent } from './home-page/home-page.component';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {routes} from './routes';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([AppEffects]),
+    CommonModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
